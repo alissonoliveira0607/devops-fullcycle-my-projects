f3672caab724917c13cb675b2cccd3a34e64227f
Alisson Oliveira <alissonoliveira0607@gmail.com>
2024-10-19 14:55:43 -0300

    Ajustado o modo de autenticação utilizado pela aplicação para a aplicação em node

--- node/index.js
+++ node/index.js
@@ -1,29 +1,40 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const mysql = require('mysql2');
+const express = require('express');
+const app = express();
+const port = 3001;
+
 const config = {
     host: 'db',
     user: 'devops',
     password: 'devops',
     database: 'devops'
 };
-const mysql = require('mysql2') // Alterado para mysql2
-const connection = mysql.createConnection(config)
 
-const sql = `INSERT INTO people(name) values('Devops')`
-connection.query(sql, (err, results) => {
-    if (err) {
-        console.error('Erro ao executar a query:', err)
-    } else {
-        console.log('Query executada com sucesso:', results)
-    }
-})
-connection.end()
+function connectWithRetry() {
+    const connection = mysql.createConnection(config);
+    
+    connection.connect(err => {
+        if (err) {
+            console.error('Erro ao conectar ao MySQL, tentando novamente em 5 segundos:', err);
+            setTimeout(connectWithRetry, 5000); // Tenta novamente em 5 segundos
+        } else {
+            console.log('Conectado ao MySQL!');
+            const sql = `INSERT INTO people(name) values('Devops')`;
+            connection.query(sql, (err, results) => {
+                if (err) console.error('Erro ao executar a query:', err);
+                else console.log('Query executada com sucesso:', results);
+            });
+            connection.end();
+        }
+    });
+}
+
+connectWithRetry();
 
 app.get('/', (req, res) => {
-    res.send('<h1>Full Cycle</h1>')
-})
+    res.send('<h1>Full Cycle</h1>');
+});
 
 app.listen(port, () => {
-    console.log('Rodando na porta ' + port)
-})
+    console.log('Rodando na porta ' + port);
+});
